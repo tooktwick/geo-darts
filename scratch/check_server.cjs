ef8eea950a0f227b644cfbb7851fe018d539136e
tooktwick <70478412+tooktwick@users.noreply.github.com>
2026-09-23 01:35:18 +0900

    chore: 検証用スクリプトおよびメモをGit管理から除外

--- scratch/check_server.cjs
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-http.get('http://localhost:5173', (res) => {
-  console.log('Status code:', res.statusCode);
-  process.exit(0);
-}).on('error', (err) => {
-  console.log('Error connecting to server:', err.message);
-  process.exit(1);
-});
